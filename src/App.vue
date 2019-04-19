3f38b6f4ec33341e027e14997b458e894d3e24ed
Fernando Jimenez <ferjiser@gmail.com>
2019-04-19 19:58:39 +0200

    vue calculator

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+    <div id="app">
+      <h1>Dinner calculator</h1>
+      <Table
+        :dinner=100
+        :tip=9
+        :people=2
+      ></Table>
+    </div>
+</template>
+
+<script>
+import Table from './components/Table.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Table
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+        color: #999;
+        font-family: 'Arial';
+
+        h1 {
+            text-align: center;
+            margin-top: 40px
+        }
+        
+}
+</style>
---
+++ src/components/Table.vue
@@ -0,0 +1,135 @@
+<template>
+<table>
+        <tbody>
+            <tr>
+                <td>Dinner</td>
+                <td>{{dinner | currency}}</td>
+                <td>
+                    <button @click="increment('dinner')">+</button>
+                    <button @click="decrement('dinner')">-</button>
+                </td>
+            </tr>
+            <tr>
+                <td>Tip</td>
+                <td>{{tip |currency}}</td>
+                <td>
+                    <button @click="increment('tip')">+</button>
+                    <button @click="decrement('tip')">-</button>
+                </td>
+            </tr>
+            <tr>
+                <td>People</td>
+                <td>{{people}}</td>
+                <td>
+                    <button @click="increment('people')">+</button>
+                    <button @click="decrement('people')">-</button>
+                </td>
+            </tr>
+            <tr>
+                <td>Total with taxes ({{this.taxes}}%)</td>
+                <td>{{totalWithTaxes | currency}} </td>
+                <td></td>
+            </tr>
+            <tr>
+                <td>Total with tip</td>
+                <td>{{totalWithTips |currency}}</td>
+                <td></td>
+            </tr>
+            <tr>
+                <td>Total per person</td>
+                <td>{{totalPerPerson | currency}}</td>
+                <td></td>
+            </tr>
+        </tbody>
+    </table>
+</template>
+
+<script>
+export default {
+  name: 'Table',
+  props: {
+    dinner: {
+        type: Number,
+        default: 0
+    },
+    tip: {
+        type: Number,
+        default: 0
+    },
+    people: {
+        type: Number,
+        default: 0
+    },
+    taxes: {
+        type: Number,
+        default: 21
+    }
+  },
+  computed: {
+        totalWithTaxes() {
+            return this.dinner*(1+this.taxes/100);
+        },
+        totalWithTips() {
+            return this.totalWithTaxes + this.tip;
+        },
+        totalPerPerson() {
+            return this.totalWithTips/this.people;
+        }
+    },
+  filters: {
+    currency(amount) {
+        const CURRENCY = '€';
+        return `${amount.toFixed(2)} ${CURRENCY}`;
+    }
+  },
+  methods: {
+        increment(type){
+            this[type]++; 
+        },
+        decrement(type){
+              this[type]--; 
+        }
+    }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+        table {
+            margin: 25px auto;
+            border-collapse: collapse;
+            border: 1px solid #eee;
+            border-bottom: 2px solid #42b883;
+            box-shadow: 0px 0px 20px rgba(0, 0, 0, 0.1),
+                0px 10px 20px rgba(0, 0, 0, 0.05),
+                0px 20px 20px rgba(0, 0, 0, 0.05),
+                0px 30px 20px rgba(0, 0, 0, 0.05);
+        }
+
+        table tr:hover {
+            background: #f4f4f4;
+        }
+
+        table tr:hover td {
+            color: #555;
+        }
+
+        table td {
+            color: #999;
+            border: 1px solid #eee;
+            padding: 12px 35px;
+            border-collapse: collapse;
+        }
+
+        table tr td:nth-child(2) {
+            text-align: right;
+        }
+
+        /* Buttons */
+
+        button {
+            padding: 4px 7px;
+            border-radius: 1px;
+            outline: none;
+        }
+</style>
